eace98bb1a29a2c75ab6cb68159d87d6ff462a6b
biagavirete <biagavirete@gmail.com>
2020-10-13 08:57:05 -0300

    style map page

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Valinhos</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <Map 
+                center={[-22.9858588,-47.0890119]}
+                zoom={13}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="http://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+        </div>
+    )
+}
+
+export default OrphanagesMap;
