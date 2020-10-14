50dbddd2b4415c93b4cc9dde7a696e24baa80bb0
biagavirete <biagavirete@gmail.com>
2020-10-14 20:27:53 -0300

    fix images upload

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -86,9 +86,7 @@ export default function Orphanage() {
                 scrollWheelZoom={false}
                 doubleClickZoom={false}
               >
-                <TileLayer 
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-                />
+                <TileLayer url="http://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                 <Marker interactive={false} icon={mapIcon} position={[orphanage.latitude,orphanage.longitude]} />
               </Map>
 
